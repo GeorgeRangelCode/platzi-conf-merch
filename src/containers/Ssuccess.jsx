d0c9727ee57fc8122874a38859267496afd804bd
jerangel <jerangel@indracompany.com>
2020-11-23 06:57:43 -0500

    Maquetación y estilos del flujo de pago

--- src/containers/Ssuccess.jsx
+++ src/containers/Ssuccess.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import '../styles/components/Success.css';
 
 const Ssuccess = () => {
   return (
-    <div>
-      <h1>Ssuccess</h1>
+    <div className="Ssuccess">
+      <div className="Ssuccess-content">
+        <h2>Jorge, Gracias por tu compra</h2>
+        <span>Tu pedido llegara en 3 dias a tu dirección:</span>
+        <div className="Ssuccess-map">Google Maps</div>
+      </div>
     </div>
   );
 };
